a0cdb448497d1e168b27b0692c4500d975f5b6e8
lhw978 <Lhw978@163.com>
2023-01-13 19:39:40 +0800

    修复旧版缺少的文件，确保VS2022编译通过06

--- all.h
+++ all.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "glad/glad.h"
-//GLAS是用来管理OpenGL的函数指针，在调用任何OpenGL函数之前我们都需要初始化GLAD
+#include "glad/glad.h" 
+//GLAS是用来管理OpenGL的 函数指针，在调用任何OpenGL函数之前我们都需要初始化 GLAD
 #include "GLFW/glfw3.h"
 #include <iostream>
 #include <cmath>
