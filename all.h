0655fc1fc4e1f6de1229ca7d35530c197f7d6df4
lhw978 <Lhw978@163.com>
2023-01-13 18:56:47 +0800

    修复旧版缺少的文件，确保VS2022编译通过03

--- all.h
+++ all.h
@@ -2,11 +2,11 @@
 #include "glad/glad.h"
 //GLAS是用来管理OpenGL的函数指针，在调用任何OpenGL函数之前我们都需要初始化GLAD
 #include "GLFW/glfw3.h"
-#include<iostream>
-#include<cmath>
-#include"openglshader.h"
-#include"camera.h"
-//#define STB_IMAGE_IMPLEMENTATION  //加这个是因为加了这个下面那个头文件才能使用 要不然他的函数就是无法解析的外部命令
+#include <iostream>
+#include <cmath>
+#include "openglshader.h"
+#include "camera.h"
+//#define STB_IMAGE_IMPLEMENTATION  //加这个是因为加了这个下面那个头文件才能使用 要不然他的函数就是无法解析的 外部命令
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtc/type_ptr.hpp" 
